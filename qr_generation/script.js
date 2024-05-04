798c43fb8d42a51ba5b536011e5efd2077b06999
Annanya Sood <87370206+Coder-anna@users.noreply.github.com>
2024-05-04 21:15:02 +0530

    QR Code Files

---
+++ qr_generation/script.js
@@ -0,0 +1,31 @@
+var fs = require('fs'); 
+
+const {Storage} = require('@google-cloud/storage');
+
+  // Creates a client
+const storage = new Storage();
+
+const storage = gcloud.storage({
+    projectId: 'minorproject-420305',
+    keyFilename: 'service-account-credentials.json',
+});
+
+const bucket = storage.bucket('minorproject-420305.appspot.com')
+
+var storage = require('@google-cloud/storage')
+// Get a reference to the storage service, which is used to create references in your storage bucket
+// var storage = firebase.storage();
+
+// Create a storage reference from our storage service
+var storageRef = storage.ref();
+
+// var file = ... // use the Blob or File API
+
+fs.readFile('1.png', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+ref.put(file).then(function(snapshot) {
+  console.log('Uploaded a blob or file!');
+});
